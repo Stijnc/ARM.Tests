8edae0ba03c9b4a54ebcc6c9c285fffef98e2a2a
Stijn Callebaut <stijnc@fooji.net>
2016-01-28 21:40:57 +0100

    redefined jsonfile

--- ARM.Tests.ps1
+++ ARM.Tests.ps1
@@ -12,8 +12,8 @@ $ErrorActionPreference = 'stop'
 Set-StrictMode -Version latest
 
 $RepoRoot = (Resolve-Path $PSScriptRoot\..).Path
-$JsonFile = Split-Path -Leaf $RepoRoot
-
+#$JsonFile = Split-Path -Leaf $RepoRoot
+$JsonFile = "$RepoRoot\azuredeploy.json"
 Describe "Json file: $JsonFile" {
    BeforeAll { 
         $SchemaVersion = '2015-01-01-preview'
@@ -30,33 +30,33 @@ Describe "Json file: $JsonFile" {
             #$powershellRepresentation = ConvertFrom-Json (Get-Content $azuredeploy -Raw)
             #should also work but throws an error for files > 2MB
             #http://stackoverflow.com/questions/17034954/how-to-check-if-file-has-valid-json-syntax-in-powershell
-           { $json = $jsser.DeserializeObject((Get-Content $azuredeploy -Raw)) } | should Not Throw       
+           { $json = $JavaScriptSerializer.DeserializeObject((Get-Content $azuredeploy -Raw)) } | should Not Throw       
         }
     } #end context Json file
     
     Context "$azuredeploy properties" {   
        BeforeAll {
-           $script:json =  $jsser.DeserializeObject((Get-Content $azuredeploy -Raw))
+           $json =  $JavaScriptSerializer.DeserializeObject((Get-Content $azuredeploy -Raw))
        }      
        It 'should include a schema' {  
-           $script:json.Keys -ccontains '$schema'| Should Be $true     
+           $json.Keys -ccontains '$schema'| Should Be $true     
        }         
         It "should have a value of $SchemaUri" {
-            $script:json['$schema'] -eq $SchemaUri | Should be $true
+            $json['$schema'] -eq $SchemaUri | Should be $true
         }      
         It 'should include parameters' {
-            $script:json.Keys -ccontains 'parameters' | Should Be $true      
+            $json.Keys -ccontains 'parameters' | Should Be $true      
         }      
         It 'should include variables' {
-            $script:json.Keys -ccontains 'variables' | Should Be $true
+            $json.Keys -ccontains 'variables' | Should Be $true
         }       
         It 'should include resources' {
-            $script:json.Keys -ccontains 'resources' | Should Be $true
+            $json.Keys -ccontains 'resources' | Should Be $true
         }      
         It 'should include outputs' {
-            $script:json.Keys -ccontains 'outputs' | Should Be $true
+            $json.Keys -ccontains 'outputs' | Should Be $true
         }       
-        foreach ($Parameter in $script:json['parameters'].GetEnumerator()) {
+        foreach ($Parameter in $json['parameters'].GetEnumerator()) {
            foreach ($Item in $Parameter) {
                $Item.Key
                It "parameter $($item.Key) contains metadata" {
@@ -65,7 +65,7 @@ Describe "Json file: $JsonFile" {
            } 
         }
         AfterAll {
-            $script:json = $null         
+            $json = $null         
         }
     } #end context properties 
 } #end describe Json file
